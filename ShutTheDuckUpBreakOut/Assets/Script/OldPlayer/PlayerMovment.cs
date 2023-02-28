7c2c27d72fc64f314d2d533d1b2a4f68e741d654
Balslev <zsefzone@gmail.com>
2023-02-28 17:24:53 +0100

    update Role
    made a role for the older version of the duck

--- ShutTheDuckUpBreakOut/Assets/Script/OldPlayer/PlayerMovment.cs
+++ ShutTheDuckUpBreakOut/Assets/Script/OldPlayer/PlayerMovment.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class PlayerMovment : MonoBehaviour
 {
-    public int speed = 150;
+
+    public float speed = 150;
     public Rigidbody2D rb;
-    UnityEngine.Vector2 Movement;
-    private float ActiveMoveSpeed;
+    [Header("RoleStats")]
+    public float RoleLeanght = 1f;
+    public float RoleForce = 750f;
     public bool Roling;
-    public  Vector2 lastDirection;
+    private  Vector2 lastDirection;
+    private float NormalMoveSpeed;
+    UnityEngine.Vector2 Movement;
   
     
     void Start()
     {
-        ActiveMoveSpeed = speed;
+        NormalMoveSpeed = speed;
         rb.GetComponent<Rigidbody2D>();
     }
 
@@ -26,43 +30,45 @@ public class PlayerMovment : MonoBehaviour
        /*  {
             anim.SetBool("Gun",false);
         } else{anim.SetBool("Gun",true); } */
-
-
-
+        
         //  Moving 
         Vector2 dir = rb.transform.position - transform.position;
         dir = dir.normalized;
         lastDirection = dir;
-        if(Roling == true){
-            
-            return;
+        if(Roling == true)
+        {
+         return;
         }
-        else{
+        else
+        {
             Movement.x = Input.GetAxisRaw("Horizontal");
             Movement.y = Input.GetAxisRaw("Vertical");
         }
         Vector2 velocity = rb.velocity;   
 
-        
         if(Input.GetKeyDown(KeyCode.Space))
         {
             if(velocity.x != 0 || velocity.y != 0)
             StartCoroutine(Roll());
         }
-        
-       
-        
     }
      IEnumerator Roll()
      {
+        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
         Roling = true;
-        speed = speed - 35;
+        speed = speed + RoleForce;
+        DOVirtual.Float( speed, 0, RoleLeanght, RolingSpeed =>{speed = RolingSpeed;});
         rb.AddForce(lastDirection * speed); 
         //change tag so it canoot be hit
-        yield return new WaitForSeconds(0.7f);
+
+        yield return new WaitForSeconds( RoleLeanght + 0.02f);
+
         gameObject.GetComponent<Collider2D>().isTrigger = false;
-        speed = speed + 35;
+
         Roling = false;  
+        speed = NormalMoveSpeed;
+
+        gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
      }
      private void FixedUpdate() {
         rb.velocity = new Vector2((Movement.x * speed) * Time.deltaTime,(Movement.y * speed) * Time.deltaTime);
